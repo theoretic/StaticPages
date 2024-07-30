4a5342c3cf6bfdac8f4322d145d9a2e5a9b2d746
theoretic <tarasov.alexei@gmail.com>
2024-07-30 20:51:00 +0500

    Info updated

--- StaticPages.info.php
+++ StaticPages.info.php
@@ -3,7 +3,7 @@
 Static pages module info
 https://github.com/ryancramerdesign/Helloworld
 AT
-28.11.23
+30.07.24
 */
 
 namespace ProcessWire;
@@ -13,18 +13,8 @@ $info = [
 	'version'				=> 012,
 	'summary'				=> 'Saves output to static html files and deletes html files on each page save.',
 	'author'				=> 'AT / atis.pro',
-	'href'					=> 'http://atis.pro',
+	'href'					=> 'http://atis.pro/en/',
 	'singular'				=> true,
 	'autoload'				=> true,
 	'icon'					=> 'file-o', 
-/*
-	'permission' => 'staticwire-generate', 
-	'permissions' => [ 'staticwire-generate' => 'Covert pages to HTML files'
-	],
-	'page' => [
-		'name' => 'staticwire',
-		'parent' => 'setup', 
-		'title' => 'Static Site Generator',
-	],
-*/
 ];
